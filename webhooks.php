a2200c3d60fee218d7e06b4c6321a05d2854f0e8
cafedcup <39214995+cafedcup@users.noreply.github.com>
2018-05-12 21:11:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -53,4 +53,11 @@ $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMX
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
 $idPush = 'U0f8ed013f50650deb6a9e0a95042d4b0';
 echo $idPush;
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 echo "END";
