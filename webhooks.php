3fc91b79f5ff88df8cc3361dc8cfcf08000d2542
cafedcup <39214995+cafedcup@users.noreply.github.com>
2018-05-12 21:09:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -52,4 +52,5 @@ echo "OK";
 $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMXf9elSUa6VQ6iAm0SiVmUxQlRbnOFN38rCMclfZ/2EfLH1O4mzPPEG8RiF3yv99r2+aRHOS+usOHxGQ882dov5owdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
 $idPush = 'U0f8ed013f50650deb6a9e0a95042d4b0';
+echo $idPush;
 echo "END";
