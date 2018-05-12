ec402f78c5cb29ff2507b14bbb7fde5d3c20ae11
cafedcup <39214995+cafedcup@users.noreply.github.com>
2018-05-12 21:26:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -41,10 +41,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			echo $ch;
+			
 			$result = curl_exec($ch);
 			curl_close($ch);
-
+			echo $messages;
 			echo $result . "\r\n";
 		}
 	}
@@ -53,7 +53,6 @@ echo "OK";
 $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMXf9elSUa6VQ6iAm0SiVmUxQlRbnOFN38rCMclfZ/2EfLH1O4mzPPEG8RiF3yv99r2+aRHOS+usOHxGQ882dov5owdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
 $idPush = 'U0f8ed013f50650deb6a9e0a95042d4b0';
-echo $idPush;
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
