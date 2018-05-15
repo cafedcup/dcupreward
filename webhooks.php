2959ce40e51b68923b3ae8bcb9a071ce47282817
cafedcup <pakawat.d@gmail.com>
2018-05-15 20:52:15 +0700

    Revert "Update"
    This reverts commit 84aec5f3efd3650759f3f1cc2957b7dcfc98869b.

--- webhooks.php
+++ webhooks.php
@@ -53,28 +53,6 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT * FROM customer_tbl WHERE schemaname='public'");
-
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
-
 
 $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMXf9elSUa6VQ6iAm0SiVmUxQlRbnOFN38rCMclfZ/2EfLH1O4mzPPEG8RiF3yv99r2+aRHOS+usOHxGQ882dov5owdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
