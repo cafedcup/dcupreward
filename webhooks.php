ef35ecc9b6835e2e86aab4d1fec40f6f22fa4455
cafedcup <pakawat.d@gmail.com>
2018-05-15 22:08:09 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -52,7 +52,33 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+$dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c22a5eb989f6da8e9aa746c30d8395f97a@ec2-54-225-200-15.compute-1.amazonaws.com:5432/d8rrl8e93ni01r")
+    or die('Could not connect: ' . pg_last_error());
 
+// Performing SQL query
+#$query = 'SELECT * FROM dcup_customer_tbl';
+$line_id = 'U0f8ed013f50650deb6a9e0a95042d4b0';
+$query = "SELECT tel FROM dcup_customer_tbl WHERE line_id = '" . $line_id . "'";
+echo $query;
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    #echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        #echo "\t\t<td>$col_value</td>\n";
+		$tel = $col_value;
+    }
+    #echo "\t</tr>\n";
+}
+#echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection 
+pg_close($dbconn);
 
 $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMXf9elSUa6VQ6iAm0SiVmUxQlRbnOFN38rCMclfZ/2EfLH1O4mzPPEG8RiF3yv99r2+aRHOS+usOHxGQ882dov5owdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
@@ -67,7 +93,7 @@ if ($response->isSucceeded()) {
     $name = $profile['displayName'];
 }
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello ' . $name);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello ' . $name ' You telephone is ' . $tel);
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
