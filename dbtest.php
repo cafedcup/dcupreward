56d1667a29e2a0a45c8ad1a95891f674f31b6e52
cafedcup <pakawat.d@gmail.com>
2018-05-15 22:00:50 +0700

    update

--- dbtest.php
+++ dbtest.php
@@ -6,7 +6,9 @@ $dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c2
 
 // Performing SQL query
 #$query = 'SELECT * FROM dcup_customer_tbl';
-$result = 'SELECT tel FROM dcup_customer_tbl WHERE line_id = ''U0f8ed013f50650deb6a9e0a95042d4b0''';
+$line_id = 'U0f8ed013f50650deb6a9e0a95042d4b0';
+$query = "SELECT tel FROM dcup_customer_tbl WHERE line_id = '" . $line_id . "'";
+echo $query;
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
