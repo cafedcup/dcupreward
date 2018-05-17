f323f8f58e0e98bb3b8d0ac0c3f22a9eb85b5579
Pakawat <pakawat.d@gmail.com>
2018-05-17 11:23:26 +0700

    update

--- dbtest.php
+++ dbtest.php
@@ -3,11 +3,13 @@
 #$dbconn = pg_connect("host=localhost dbname=publishing user=www password=foo")
 $dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c22a5eb989f6da8e9aa746c30d8395f97a@ec2-54-225-200-15.compute-1.amazonaws.com:5432/d8rrl8e93ni01r")
     or die('Could not connect: ' . pg_last_error());
-echo 'before';
-is_lineid_exist($dbconn,'1');
-insert_customer($dbconn,'','1','');
-is_lineid_exist($dbconn,'1');
-echo 'after';
+$cus_line_id = '2';
+$cus_tel = '888';
+if (!is_lineid_exist($dbconn,$cus_line_id))
+{
+    insert_customer($dbconn,'',$cus_line_id,$cus_tel);
+}
+
 function insert_customer($dbconn,$cus_id,$cus_line_id,$cus_tel){
     $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => '','cus_line_id' => $cus_line_id,'cus_tel' => $cus_tel)) or die('Query failed: ' . pg_last_error());
     
@@ -38,7 +40,7 @@ function is_lineid_exist($dbconn,$cus_line_id){
             $cus_line_id = $col_value;
         }
     }
-    echo $cus_line_id != '';
+    return $cus_line_id != '';
     // Free resultset
     pg_free_result($result);
     // Closing connection 
