111b14d77f9bc06f8794dab8e73f9093c04bf986
Pakawat <pakawat.d@gmail.com>
2018-05-17 12:18:17 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -9,7 +9,7 @@ $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMX
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
+$isPhoneText = false;
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -21,9 +21,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
+			#$isPhoneText = isPhone($event['message']['text']);
 			$cus_line_id = $event['source']['userId'];
 			$cus_line_name = $event['source']['displayName'];
-			
+			$tel = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -57,6 +58,15 @@ if (!is_null($events['events'])) {
 	}
 }
 
+function isPhone($string) {
+	$isPhone = false;
+    $numbersOnly = ereg_replace("[^0-9]", "", $string);
+    $numberOfDigits = strlen($numbersOnly);
+    if ($numberOfDigits == 10) {
+        $isPhone = true;
+    }
+    retrun $isPhone;
+}
 
 function insert_customer($dbconn,$cus_line_id){
     $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => '','cus_line_id' => $cus_line_id)) or die('Query failed: ' . pg_last_error());
@@ -93,21 +103,41 @@ function is_lineid_exist($dbconn,$cus_line_id){
     pg_free_result($result);
     // Closing connection 
 }
-$hello = 'Hello ';
-if (!is_lineid_exist($dbconn,$cus_line_id))
-{
-    insert_customer($dbconn,$cus_line_id);
-    $hello = 'Wellcome frist time ';
+function is_cudtel_exist($dbconn,$cus_line_id){
+    $query = "SELECT cus_tel FROM dcup_customer_mst WHERE cus_line_id = '" . $cus_line_id . "'";
+    $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        foreach ($line as $col_value) {        
+            $custel = $col_value;
+        }
+    }
+    return $custel != '';
+    // Free resultset
+    pg_free_result($result);
+    // Closing connection 
 }
-
-
 $response = $bot->getProfile($cus_line_id);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     $name = $profile['displayName'];
 }
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($hello . $name . ' Your telephone is ' . $tel);
+$hello = 'Hello ';
+if (!is_lineid_exist($dbconn,$cus_line_id))
+{
+    insert_customer($dbconn,$cus_line_id);
+    $hello = 'Wellcome frist time ';
+    $tel = 'Plese enter your telephone number';
+}
+#elseif (!is_lineid_exist($dbconn,$cus_line_id))
+#{
+
+#}
+
+
+
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($hello . $name . $tel);
 $response = $bot->pushMessage($cus_line_id, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
