e3b99e6cb8597c4d85c3a50146bcffb302ac40f4
Pakawat <pakawat.d@gmail.com>
2018-05-17 10:20:44 +0700

    update

--- dbtest.php
+++ dbtest.php
@@ -4,15 +4,15 @@
 $dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c22a5eb989f6da8e9aa746c30d8395f97a@ec2-54-225-200-15.compute-1.amazonaws.com:5432/d8rrl8e93ni01r")
     or die('Could not connect: ' . pg_last_error());
 
-#$max_id = getmax_id($dbconn);
-insert_customer($dbconn,2,'','');
+$max_id = getmax_id($dbconn);
+insert_customer($dbconn,max_id+1,'','');
 
 function insert_customer($dbconn,$cus_id,$cus_line_id,$cus_tel){
     $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => $cus_id,'cus_line_id' => $cus_line_id,'cus_tel' => $cus_tel)) or die('Query failed: ' . pg_last_error());
     
     pg_free_result($result);
     // Closing connection 
-    pg_close($dbconn);    
+     
 }
 
 function getmax_id($dbconn){
@@ -26,7 +26,6 @@ function getmax_id($dbconn){
     // Free resultset
     pg_free_result($result);
     // Closing connection 
-    pg_close($dbconn);
     return $max_id;
 }
 
@@ -34,5 +33,5 @@ function getmax_id($dbconn){
 
 // Printing results in HTML
 
-
+ pg_close($dbconn);  
 ?>
