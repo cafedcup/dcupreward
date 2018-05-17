813f9bf4c11de21dea7d7d1f65bff55153eb08be
Pakawat <pakawat.d@gmail.com>
2018-05-17 10:15:31 +0700

    update

--- dbtest.php
+++ dbtest.php
@@ -4,17 +4,13 @@
 $dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c22a5eb989f6da8e9aa746c30d8395f97a@ec2-54-225-200-15.compute-1.amazonaws.com:5432/d8rrl8e93ni01r")
     or die('Could not connect: ' . pg_last_error());
 
-getmax_id($dbconn);
+$max_id = getmax_id($dbconn);
+insert_customer($dbconn,$max_id+1,'','');
 
-function insert_customer($dbconn){
-    $cus_id = 1;
-    $cus_name = 'Pakawat';
-    $cus_line_id = 'U0f8ed013f50650deb6a9e0a95042d4b0';
-    #$cus_hbdate = ;
-    #$cus_regdate = date();
-    #$cus_sex =     
-    $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => $cus_id,'cus_name' => $cus_name,'cus_line_id' => $cus_line_id)) or die('Query failed: ' . pg_last_error());
-        pg_free_result($result);
+function insert_customer($dbconn,$cus_id,$cus_line_id,$cus_tel){
+    $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => $cus_id,'cus_line_id' => $cus_line_id,'cus_tel' => $cus_tel)) or die('Query failed: ' . pg_last_error());
+    
+    pg_free_result($result);
     // Closing connection 
     pg_close($dbconn);    
 }
@@ -24,14 +20,14 @@ function getmax_id($dbconn){
     $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
     while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
         foreach ($line as $col_value) {        
-            $max = $col_value;
+            $max_id = $col_value;
         }
     }
-    echo $max;
     // Free resultset
     pg_free_result($result);
     // Closing connection 
     pg_close($dbconn);
+    return $max_id;
 }
 
 #$result = pg_update($dbconn,'dcup_customer_tbl',array('entry_count' => $entry),array('name' => $username)) or die('Query failed: ' . pg_last_error());
