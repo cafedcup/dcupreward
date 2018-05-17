b2d76c6d25d782f4a3fa99b9075bb72b52f94b48
Pakawat <pakawat.d@gmail.com>
2018-05-17 09:45:02 +0700

    update

--- dbtest.php
+++ dbtest.php
@@ -4,44 +4,31 @@
 $dbconn = pg_connect("postgres://iesaxpzthmoosu:2985fd62590b6987485efe84c96dc5c22a5eb989f6da8e9aa746c30d8395f97a@ec2-54-225-200-15.compute-1.amazonaws.com:5432/d8rrl8e93ni01r")
     or die('Could not connect: ' . pg_last_error());
 
-// Performing SQL query
-#$query = 'SELECT * FROM dcup_customer_tbl';
-$line_id = 'U0f8ed013f50650deb6a9e0a95042d4b0';
-#$insert_str = "UPDATE dcup_customer_tbl entry_count=1 WHERE name = 'Pakrub'";
-#echo $insert_str;
-$cus_id = 1;
-$cus_name = 'Pakawat';
-$cus_line_id = 'Test';
-#$cus_hbdate = ;
-#$cus_regdate = date();
-#$cus_sex = 
+getmax_id();
+
+function insert_customer($dbconn){
+    $cus_id = 1;
+    $cus_name = 'Pakawat';
+    $cus_line_id = 'U0f8ed013f50650deb6a9e0a95042d4b0';
+    #$cus_hbdate = ;
+    #$cus_regdate = date();
+    #$cus_sex =     
+    $result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => $cus_id,'cus_name' => $cus_name,'cus_line_id' => $cus_line_id)) or die('Query failed: ' . pg_last_error());    
+}
+
+function getmax_id(){
+    $query = "SELECT max(cus_id) FROM dcup_customer_mst";
+    $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
+    echo $result[0];
+    // Free resultset
+    pg_free_result($result);
+    // Closing connection 
+    pg_close($dbconn);
+}
+
 #$result = pg_update($dbconn,'dcup_customer_tbl',array('entry_count' => $entry),array('name' => $username)) or die('Query failed: ' . pg_last_error());
-$result = pg_insert($dbconn,'dcup_customer_mst',array('cus_id' => $cus_id,'cus_name' => $cus_name,'cus_line_id' => $cus_line_id)) or die('Query failed: ' . pg_last_error());
-#$query = "SELECT tel FROM dcup_customer_tbl WHERE line_id = '" . $line_id . "'";
-#echo $query;
-#$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
-echo $result;
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    #echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        #echo "\t\t<td>$col_value</td>\n";
-		$tel = $col_value;
-		echo $tel;
-    }
-    #echo "\t</tr>\n";
-}
-#echo "</table>\n";
-if ($tel == ''){
-	echo 'Null';
-}
 
-	
-// Free resultset
-pg_free_result($result);
 
-// Closing connection 
-pg_close($dbconn);
 ?>
