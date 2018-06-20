f99391ed42fc16d025fac7afac07b057e2d250b1
cafedcup <pakawat.d@gmail.com>
2018-06-20 12:03:20 +0700

    update

--- include/Database.php
+++ include/Database.php
@@ -105,11 +105,11 @@
 			mysql_close($this->dbLink);
 			$this->dbLink = false;		
 		}
-		/*
+		
 		public function get_admin_pw($user_name){
 		    $query = "SELECT admin_pw FROM dcup_admin_mst WHERE admin_name = '" . $user_name . "'";
-		    $result = self::pg_query(this->dbLink,$query) or die('Query failed: ' . self::pg_last_error());
-		    while ($line = self::pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		    $result = pg_query(this->dbLink,$query) or die('Query failed: ' . pg_last_error());
+		    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		        foreach ($line as $col_value) {        
 		            $admin_pw = $col_value;
 		        }
@@ -118,7 +118,7 @@
 		    self::pg_free_result($result);
 		    return $admin_pw;
 		}
-	
+		/*
 		public function is_admin_exist($user_name){
 		    $query = "SELECT * FROM dcup_admin_mst WHERE admin_name = '" . $user_name . "'";
 		    $result = pg_query(this->dbLink,$query) or die('Query failed: ' . pg_last_error());
